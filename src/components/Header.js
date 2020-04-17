5ea1971eed792b7995648a9e5531b3aee3593bbb
Ajil Abdalqader <ajilqado@gmail.com>
2020-04-18 01:31:07 +0200

    creating-Header-Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="top">
+            <div>
+            <img src="" alt=""/>
+            </div>
+            <h2>Awesome Company</h2>
+            </div>
+            <div>
+            <h1>Team Members</h1>
+            </div>
+        </div>
+    )
+}
